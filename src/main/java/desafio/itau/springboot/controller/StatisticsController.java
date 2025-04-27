063f34d27540e2276da101550060e5f0c622f2fa
Thiago Boneth <t_boneth@hotmail.com>
2025-04-27 01:18:30 -0400

    Service e controles

---
+++ src/main/java/desafio/itau/springboot/controller/StatisticsController.java
@@ -0,0 +1,28 @@
+package desafio.itau.springboot.controller;
+
+import desafio.itau.springboot.dto.StatisticsResponse;
+import desafio.itau.springboot.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.DoubleSummaryStatistics;
+
+@RestController
+@RequestMapping("estatistica")
+public class StatisticsController {
+
+    private final TransactionService transactionService;
+
+    public StatisticsController(final TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticsResponse> getStatistics() {
+        DoubleSummaryStatistics stats = transactionService.getStatistics();
+
+        return ResponseEntity.ok(new StatisticsResponse(stats));
+    }
+}
