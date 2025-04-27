c44bb99333dd4df8c61d6ba3f75b35013c5c4bf8
Thiago Boneth <t_boneth@hotmail.com>
2025-04-27 01:16:59 -0400

    Estrutura de projeto

---
+++ src/main/java/desafio/itau/springboot/DesafioJavaItauApplication.java
@@ -0,0 +1,13 @@
+package desafio.itau.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaItauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioJavaItauApplication.class, args);
+    }
+
+}
